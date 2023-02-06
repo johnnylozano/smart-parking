dd116201e6d59a7114a72aea5c7d66dd74cf09ca
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2023-02-05 23:27:33 -0500

    feat: Updated dependencies and added routes
    Installed react-router-dom. Created home, search, and parking pages. Setup router to navigate to new pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from "src/pages/home";
+import { Search } from "src/pages/search";
+import { Parking } from "src/pages/parking";
 
 function App() {
+  const Root = () => {
+    return (
+      <>
+        {/* temp navbar */}
+        <div className="">
+          <Link to="/">Home</Link>
+          <Link to="/search">Search</Link>
+          <Link to="/parking">Parking</Link>
+        </div>
+        <div>
+          <Outlet />
+        </div>
+      </>
+    );
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/parking" element={<Parking />} />
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/parking.js
@@ -0,0 +1,3 @@
+export const Parking = () => {
+  return <h1>Parking</h1>;
+};
