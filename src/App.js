7ae796011758cf60d57dfe147da46f0796a3eb71
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2023-03-13 12:21:21 -0400

    refactor: unused function

--- src/App.js
+++ src/App.js
@@ -13,59 +13,12 @@ import { Parking } from "src/pages/parking";
 import Navbar from "./components/Navbar";
 import { SignIn } from "./pages/signin";
 import Footer from "./components/Footer";
-import { AuthProvider, AuthContext } from "./context/AuthProvider";
+import { AuthProvider } from "./context/AuthProvider";
 // AWS Auth
 import { Amplify } from "aws-amplify";
 import awsconfig from "./aws-exports";
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { useContext } from "react";
 Amplify.configure(awsconfig);
-
-function PrivateRoute({ element, ...rest }) {
-  const { isAuthenticated } = useContext(AuthContext);
-
-  return isAuthenticated ? (
-    element
-  ) : (
-    <Authenticator
-      signUpConfig={{
-        signUpFields: [
-          {
-            label: "KSU ID",
-            type: "custom:ksuId",
-            required: true,
-            placeholder: "Enter your KSU ID",
-          },
-          {
-            label: "Car Make",
-            type: "custom:CarMake",
-            required: true,
-            placeholder: "Enter your car make",
-          },
-          {
-            label: "Car Model",
-            type: "custom:CarModel",
-            required: true,
-            placeholder: "Enter your car model",
-          },
-          {
-            label: "Car Year",
-            type: "custom:CarYear",
-            required: true,
-            placeholder: "Enter your car year",
-          },
-          {
-            label: "Color",
-            type: "custom:color",
-            required: true,
-            placeholder: "Enter your car color",
-          },
-        ],
-      }}
-    />
-  );
-}
 
 function App() {
   const Root = () => {
