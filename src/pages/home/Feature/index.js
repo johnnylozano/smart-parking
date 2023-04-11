42737f86e89210f75329e7f1a5308a93091bd986
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2023-04-11 12:10:16 -0400

    fix: missing alt tags in home/feature section

--- src/pages/home/Feature/index.js
+++ src/pages/home/Feature/index.js
@@ -18,21 +18,21 @@ const Feature = () => {
       <FeaturesH1>Our Features</FeaturesH1>
       <FeaturesWrapper>
         <FeaturesCard>
-          <FeaturesIcon src={Icon1} />
+          <FeaturesIcon src={Icon1} alt="Mobile App" />
           <FeaturesH2>Real-time Availability</FeaturesH2>
           <FeaturesP>
             Instantly know where parking spots are available.
           </FeaturesP>
         </FeaturesCard>
         <FeaturesCard>
-          <FeaturesIcon src={Icon2} />
+          <FeaturesIcon src={Icon2} alt="Globe" />
           <FeaturesH2>Global Accessibility</FeaturesH2>
           <FeaturesP>
             Access our platform online anywhere in the world.
           </FeaturesP>
         </FeaturesCard>
         <FeaturesCard>
-          <FeaturesIcon src={Icon3} />
+          <FeaturesIcon src={Icon3} alt="Relaxing by vehicle" />
           <FeaturesH2>Navigation Assistance</FeaturesH2>
           <FeaturesP>
             Get turn-by-turn directions to available parking spots.
